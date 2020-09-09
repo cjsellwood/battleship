70c8a5bd8fcd9a516f190034db10773da2df67d9
cjsellwood <c.sellwood22@gmail.com>
2020-09-09 12:15:57 +1000

    Computer will follow pattern if 2 hits in a row

--- src/Factories/Player.js
+++ src/Factories/Player.js
@@ -13,7 +13,6 @@ const Player = (name) => {
 
   // Randomly choose adjacent square to last hit
   const adjacentAttack = (opposition, lastAttack) => {
-    console.log("adjacent");
     let newAttack = Math.random();
     let row = -1;
     let column = -1;
@@ -21,7 +20,6 @@ const Player = (name) => {
     console.log(row, column);
     while (row < 1 || row > 10 || column < 1 || column > 10) {
       newAttack = Math.random();
-      console.log(newAttack)
       row = lastAttack.row;
       column = lastAttack.column;
       if (newAttack < 0.25) {
@@ -35,11 +33,11 @@ const Player = (name) => {
       }
     }
 
-    console.log(row, column);
     let result = opposition.gameboard.receiveAttack(row, column);
-    console.log(result)
     let i = 0;
     while (result === false) {
+      row = -1;
+      column = -1;
       while (row < 1 || row > 10 || column < 1 || column > 10) {
         newAttack = Math.random();
         row = lastAttack.row;
@@ -55,12 +53,52 @@ const Player = (name) => {
         }
       }
       result = opposition.gameboard.receiveAttack(row, column);
-      console.log(result);
+
       i += 1;
       if (i === 100) {
         return false;
       }
     }
+
+    let lastHit = {
+      row,
+      column,
+      result,
+    };
+    hits.push(lastHit);
+    return result;
+  };
+
+  const sequentialAttack = (opposition, lastAttack, secondLast) => {
+    let row,
+      column = null;
+
+    // If two in a row horizontally
+    if (lastAttack.row === secondLast.row) {
+      row = lastAttack.row;
+      if (lastAttack.column > secondLast.column) {
+        column = lastAttack.column + 1;
+      } else {
+        column = lastAttack.column - 1;
+      }
+      // If two in a row vertically
+    } else if (lastAttack.column === secondLast.column) {
+      column = lastAttack.column;
+      if (lastAttack.row > secondLast.row) {
+        row = lastAttack.row + 1;
+      } else {
+        row = lastAttack.row - 1;
+      }
+      // If not either return
+    } else {
+      return false;
+    }
+    // If outside bounds return
+    if (row > 10 || row < 1 || column > 10 || column < 1) {
+      return false;
+    }
+
+    let result = opposition.gameboard.receiveAttack(row, column);
 
     let lastHit = {
       row,
@@ -91,22 +129,44 @@ const Player = (name) => {
   };
 
   const computerAttack = (opposition) => {
-    console.log(hits);
-    if (hits.length !== 0) {
+    // Random first attack
+    if (hits.length === 0) {
+      randomAttack(opposition);
+
+      // Second attack is either adjacent or random
+    } else if (hits.length === 1) {
       let lastAttack = hits[hits.length - 1];
-      // Attack adjacent spot if last attack was a hit
       if (lastAttack.result === "Hit") {
-        if(!adjacentAttack(opposition, lastAttack)) {
-          randomAttack(opposition)
-        };
+        // If can't find available adjacent do random attack
+        if (!adjacentAttack(opposition, lastAttack)) {
+          randomAttack(opposition);
+          return;
+        }
       } else {
         randomAttack(opposition);
       }
+      // Attack next spot in pattern if two hits consecutively
     } else {
-      randomAttack(opposition);
+      let lastAttack = hits[hits.length - 1];
+      let secondLast = hits[hits.length - 2];
+      if (lastAttack.result === "Hit" && secondLast.result === "Hit") {
+        // If no pattern found do random attack
+        if (!sequentialAttack(opposition, lastAttack, secondLast)) {
+          randomAttack(opposition);
+          return;
+        }
+      } else {
+        if (lastAttack.result === "Hit") {
+          // If can't find available adjacent do random attack
+          if (!adjacentAttack(opposition, lastAttack)) {
+            randomAttack(opposition);
+            return;
+          }
+        } else {
+          randomAttack(opposition);
+        }
+      }
     }
-
-    // Keep track of computer attacks
   };
   return {
     getName,
